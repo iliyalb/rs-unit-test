67ba878c38349c858842095fd33cab5b8f2acadc
ILIYA LESANI <iliyalesani@gmail.com>
2019-10-27 13:55:28 +0330

    Add project files.

---
+++ ConsoleApp_UnitTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_UnitTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string GetFullName(string firstName, string lastName)
+        {
+            return (firstName.Trim() + " " + lastName.Trim()).Trim(); 
+        }
+    }
+}
---
+++ UnitTest/ProjectTest2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MathLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ProjectTest2
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            // Arrange
+            CustomMath oMath = new CustomMath(7, 5);
+
+            // Act
+            int result = oMath.Add();
+
+            // Assert
+            Assert.AreEqual<int>(12, result);
+        }
+
+        [TestMethod]
+        public void TestSub()
+        {
+            // Arrange
+            CustomMath oMath = new CustomMath(10, 3);
+
+            // Act
+            int result = oMath.Sub();
+
+            // Assert
+            Assert.AreEqual<int>(7, result);
+        }
+    }
+}
+
